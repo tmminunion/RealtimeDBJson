94d9389446a525651e258276e329703aef9c52dd
tmmin <tmminunion@gmail.com>
2025-05-27 18:12:55 +0700

    yeah nebrjkb sjk bj rb

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const http = require("http");
 const app = require("./app");
-const {handleWebSocket }= require("./services/websocketHandler");
+const { handleWebSocket } = require("./services/websocketHandler");
 
 const PORT = process.env.PORT || 8008;
 const server = http.createServer(app);
 
-server.listen(PORT,'0.0.0.0', () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`HTTP server running on http://localhost:${PORT}`);
 });
 
